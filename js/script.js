be0c1eca985d2e31bc9eb9ce5b859b990027af0d
jabarjung <jabarjung@yahoo.ca>
2017-11-07 14:57:02 -0800

    Initial setup for script.js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+(function() {
+  const apiLocation = 'https://api.unsplash.com/';
+  const apiSecret =
+    'ed02bb81fc67a17e13c5118ae29fa3954daaa3a7511a4905743e99ba80650893';
+  // Listen for the event when the user clicks on the 'Search' button
+  document.querySelector('.queryButton').addEventListener(
+    'click',
+    function(e) {
+      e.preventDefault();
+      const queryField = document.querySelector('#queryField').value;
+      // Empting the search box
+      document.querySelector('#queryField').value = '';
+      // Getting response from Unsplash
+      fetch()
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(response) {
+          successfulResponse(response);
+        })
+        .catch(function() {
+          failedResponse();
+        });
+    },
+    false,
+  );
+  function successfulResponse(response) {
+    // Function definition will go here
+  }
+  function failedResponse() {
+    document.querySelector('.searchResults').textContent =
+      'Error processing your request.';
+  }
+})();
