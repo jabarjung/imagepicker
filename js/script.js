e3dacc2e0618548bf9af6f7c0b31cf93228df5fa
jabarjung <jabarjung@yahoo.ca>
2017-11-07 19:26:37 -0800

    Successful fetch request is sent

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 'use strict';
 (function() {
   const apiLocation = 'https://api.unsplash.com/';
+  const appID =
+    '168eb60857bcabe273a4b3fa67d8fb5a62e50f8dc39e0604e8443d00d6c963cf';
   const apiSecret =
     'ed02bb81fc67a17e13c5118ae29fa3954daaa3a7511a4905743e99ba80650893';
+  const pageNo = '1';
+  const itemsPerPage = '20';
   // Listen for the event when the user clicks on the 'Search' button
   document.querySelector('.queryButton').addEventListener(
     'click',
@@ -12,7 +16,17 @@
       // Empting the search box
       document.querySelector('#queryField').value = '';
       // Getting response from Unsplash
-      fetch()
+      fetch(
+        apiLocation +
+          'search/photos/?page=' +
+          pageNo +
+          '&per_page=' +
+          itemsPerPage +
+          '&query=' +
+          queryField +
+          '&client_id=' +
+          appID,
+      )
         .then(function(response) {
           return response.json();
         })
@@ -27,8 +41,10 @@
   );
   function successfulResponse(response) {
     // Function definition will go here
+    console.log('It works');
   }
   function failedResponse() {
+    console.log("Doesn't work");
     document.querySelector('.searchResults').textContent =
       'Error processing your request.';
   }
